a10478a3f517f0a9429f2d3be7b5d646565a4c3a
priyanka97rk <32797951+priyanka97rk@users.noreply.github.com>
2018-04-25 01:38:15 +0530

    added color

--- frame1.java
+++ frame1.java
@@ -39,6 +39,7 @@ public class frame1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		getContentPane().setBackground( Color.red );
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
